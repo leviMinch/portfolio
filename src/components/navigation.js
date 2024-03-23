98717b6cec16422886ac33bb913b523a8b157c5d
Levi Minch <minchle@oregonstate.edu>
2024-03-22 20:56:16 -0700

    starting work

---
+++ src/components/navigation.js
@@ -0,0 +1,18 @@
+import link from 'react-router-dom';
+
+function Navigation(){
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/users">Users</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
