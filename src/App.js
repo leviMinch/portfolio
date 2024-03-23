2ea011db340dd5c93f621be9e1752276b7fe94de
Levi Minch <minchle@oregonstate.edu>
2024-03-22 21:12:38 -0700

    working on route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import Navigation from './components/navigation';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = function () {
 	return (
 		<div>
-			<Navigation />
+			<Router>
+				<Navigation />
+			</Router>
 			<h1>Home</h1>
 		</div>
 	)
